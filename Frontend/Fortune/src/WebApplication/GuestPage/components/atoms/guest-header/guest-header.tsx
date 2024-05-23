235ad5059c10728e0ee2f82f8f1cae32f6e884f4
TangDuYen <YenTDSE171310@fpt.edu.vn>
2024-05-23 13:51:50 +0700

    update fe guest page

--- Frontend/Fortune/src/WebApplication/GuestPage/components/atoms/guest-header/guest-header.tsx
+++ Frontend/Fortune/src/WebApplication/GuestPage/components/atoms/guest-header/guest-header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link as Forward } from 'react-router-dom';
 import './guest-header.scss';
 import logo from '../../../../../../src/assets/img/logo2.png';
 import account from '../../../../../../src/assets/img/account.png';
+import { Link } from 'react-scroll'
 
 const GuestHeader: React.FC = () => {
   return (
@@ -13,22 +14,30 @@ const GuestHeader: React.FC = () => {
             <img src={logo} alt="logo" className='logo' />
           </div>
           <div className="guest-header-items">
-            <li className="inline-block">HOME</li>
+            <Link to='guest-introduction'>
+              <li className="inline-block">HOME</li>
+            </Link>
           </div>
           <div className="guest-header-items">
-            <li className="inline-block">AUCTIONS</li>
+            <Link to='auctions-content'>
+              <li className="inline-block">AUCTIONS</li>
+            </Link>
           </div>
           <div className="guest-header-items">
-            <li className="inline-block">RULES</li>
+            <Link to='jewel-content'>
+              <li className="inline-block">JEWELRY</li>
+            </Link>
           </div>
           <div className="guest-header-items">
-            <li className="inline-block">ABOUT US</li>
+            <Link to='auctions-rule'>
+              <li className="inline-block">RULE</li>
+            </Link>
           </div>
           <div className="guest-header-items">
             <li className="inline-block">
-              <Link to='/login'>
+              <Forward to='/login'>
                 <img src={account} alt="account" className='account' />
-              </Link>
+              </Forward>
             </li>
           </div>
         </ul>
