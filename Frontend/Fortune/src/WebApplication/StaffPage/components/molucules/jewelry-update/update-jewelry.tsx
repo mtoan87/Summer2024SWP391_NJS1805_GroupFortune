77fc4cd53bc9c8254796c196fb5886ce26f0327e
hiepnt171646 <97937052+hiepnt171646@users.noreply.github.com>
2024-06-21 12:46:53 +0700

    fix create

--- Frontend/Fortune/src/WebApplication/StaffPage/components/molucules/jewelry-update/update-jewelry.tsx
+++ Frontend/Fortune/src/WebApplication/StaffPage/components/molucules/jewelry-update/update-jewelry.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import api from '../../../../../config/axios';
 import './update-jewelry.scss';
@@ -7,7 +7,6 @@ function ViewJewelryDetails() {
   const [jewelryDetails, setJewelryDetails] = useState({
     accountId: '',
     imageUrl: '',
-    imageFile: null,
     name: '',
     materials: '',
     description: '',
@@ -18,18 +17,19 @@ function ViewJewelryDetails() {
     purity: '',
     price: '',
     collection: '',
-    jewelryImg: ''
+    jewelryImg: '' // Initialize as empty string
   });
   const [errors, setErrors] = useState({});
   const { id, material } = useParams();
-  const fileInputRef = useRef(null);
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchJewelryDetails = async () => {
       try {
         const response = await api.get(`/api/Jewelry${material === 'gold' ? 'Gold' : 'Silver'}/GetById/${id}`);
-        setJewelryDetails({ ...response.data });
+        setJewelryDetails({
+          ...response.data
+        });
       } catch (error) {
         console.error('Error fetching jewelry details:', error);
       }
@@ -38,33 +38,12 @@ function ViewJewelryDetails() {
     fetchJewelryDetails();
   }, [id, material]);
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
+  const handlePriceChange = (e) => {
+    const { value } = e.target;
     setJewelryDetails(prevState => ({
       ...prevState,
-      [name]: value
+      price: value
     }));
-  };
-
-  const handleImageClick = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
-  };
-
-  const handleImageUpload = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setJewelryDetails(prevState => ({
-          ...prevState,
-          imageUrl: reader.result,
-          imageFile: file
-        }));
-      };
-      reader.readAsDataURL(file);
-    }
   };
 
   const handleUpdateJewelry = async () => {
@@ -77,14 +56,11 @@ function ViewJewelryDetails() {
     formData.append('Weight', jewelryDetails.weight);
     formData.append('Price', jewelryDetails.price);
     formData.append('WeightUnit', jewelryDetails.weightUnit);
+    formData.append('jewelryImg', jewelryDetails.jewelryImg);
     if (jewelryDetails.materials === 'gold') {
       formData.append('GoldAge', jewelryDetails.goldAge);
     } else {
       formData.append('Purity', jewelryDetails.purity);
-    }
-
-   if (jewelryDetails.jewelryImg) {
-      formData.append('JewelryImg', jewelryDetails.jewelryImg);
     }
 
     try {
@@ -110,94 +86,43 @@ function ViewJewelryDetails() {
       <div className="jewelry-details-container">
         <div className="jewelry-details-item">
           <label htmlFor="image">Image</label>
-          <div className="upload-label-details" onClick={handleImageClick}>
+          <div className="upload-label-details">
             <img className='item-img'
-              src={jewelryDetails.imageUrl || `https://localhost:44361/${jewelryDetails.jewelryImg}`}
+              src={`https://localhost:44361/${jewelryDetails.jewelryImg}`}
               alt={jewelryDetails.name}
               onError={(e) => { e.target.src = "src/assets/img/jewelry_introduction.jpg"; }}
             />
-            <div className="upload-text-details">Upload Image</div>
-            <input
-              ref={fileInputRef}
-              type="file"
-              id="image"
-              name="image"
-              onChange={handleImageUpload}
-              accept="image/*"
-              style={{ display: 'none' }}
-            />
           </div>
+
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" value={jewelryDetails.name} onChange={handleInputChange} />
-          {errors.name && <span className="error">{errors.name}</span>}
-          
-          <label htmlFor="weight">Weight</label>
-          <div className="input-container">
-            <input type="text" name="weight" value={jewelryDetails.weight} onChange={handleInputChange} />
-            <select
-              className="weight-unit-select"
-              name="weightUnit"
-              value={jewelryDetails.weightUnit}
-              onChange={handleInputChange}
-            >
-              <option value="grams">g</option>
-              <option value="kilograms">kg</option>
-              <option value="ounces">oz</option>
-              <option value="pounds">lb</option>
-            </select>
-          </div>
-          {errors.weight && <span className="error">{errors.weight}</span>}
-          
-          <label htmlFor="price">Price</label>
-          <div className="input-container">
-            <input type="text" name="price" value={jewelryDetails.price} onChange={handleInputChange} />
-          </div>
-          {errors.price && <span className="error">{errors.price}</span>}
-          
-          <label htmlFor="description">Description</label>
-          <textarea name="description" value={jewelryDetails.description} onChange={handleInputChange} />
-          {errors.description && <span className="error">{errors.description}</span>}
+          <input type="text" name="name" value={jewelryDetails.name} disabled />
 
           <label htmlFor="materials">Materials</label>
-          <select name="materials" value={jewelryDetails.materials} onChange={handleInputChange}>
-            <option value="">Select Material</option>
-            <option value="gold">Gold</option>
-            <option value="silver">Silver</option>
-            <option value="platinum">Platinum</option>
-            <option value="diamond">Diamond</option>
-          </select>
-          {errors.materials && <span className="error">{errors.materials}</span>}
-          
+          <input type="text" name="materials" value={jewelryDetails.materials} disabled />
+
+          <label htmlFor="category">Category</label>
+          <input type="text" name="category" value={jewelryDetails.category} disabled />
+
           {jewelryDetails.materials === 'gold' && (
-            <> <label htmlFor="goldAge">Gold Age</label>
-            <div className="input-container">
-             
-              <input type="text" name="goldAge" value={jewelryDetails.goldAge} onChange={handleInputChange} />
-              {errors.goldAge && <span className="error">{errors.goldAge}</span>}
-              <span className="suffix">K</span>
-            </div></>
+            <>
+              <label htmlFor="goldAge">Gold Age</label>
+              <input type="text" name="goldAge" value={jewelryDetails.goldAge} disabled />
+            </>
           )}
 
           {jewelryDetails.materials === 'silver' && (
-            <> <label htmlFor="purity">Purity</label>
-            <div className="input-container">
-             
-              <input type="text" name="purity" value={jewelryDetails.purity} onChange={handleInputChange} />
-              {errors.purity && <span className="error">{errors.purity}</span>}
-              <span className="suffix">%</span>
-            </div></>
+            <>
+              <label htmlFor="purity">Purity</label>
+              <input type="text" name="purity" value={jewelryDetails.purity} disabled />
+            </>
           )}
 
-          <label htmlFor="category">Category</label>
-          <select name="category" value={jewelryDetails.category} onChange={handleInputChange}>
-            <option value="">Select Category</option>
-            <option value="necklace">Necklace</option>
-            <option value="ring">Ring</option>
-            <option value="bracelet">Bracelet</option>
-            <option value="earrings">Earrings</option>
-          </select>
-          {errors.category && <span className="error">{errors.category}</span>}
-          
+          <label htmlFor="price">Price</label>
+          <div className="input-container">
+            <input type="text" name="price" value={jewelryDetails.price} onChange={handlePriceChange} />
+          </div>
+          {errors.price && <span className="error">{errors.price}</span>}
+
           <button onClick={handleUpdateJewelry}>Update</button>
         </div>
       </div>
