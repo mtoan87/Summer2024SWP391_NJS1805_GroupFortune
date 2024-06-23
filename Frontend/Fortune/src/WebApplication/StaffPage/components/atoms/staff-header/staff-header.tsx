d4a6ac056f79ee109fad6d28c9291e20421d6de0
St-Louder <lyvanmy357@gmail.com>
2024-06-23 23:32:25 +0700

    Fix css staff again

--- Frontend/Fortune/src/WebApplication/StaffPage/components/atoms/staff-header/staff-header.tsx
+++ Frontend/Fortune/src/WebApplication/StaffPage/components/atoms/staff-header/staff-header.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './member-header.scss';
 import logo from '../../../../../../src/assets/img/logo2.png';
-import { Link as ScrollLink } from 'react-scroll';
 import { FaRegUser } from "react-icons/fa";
 import Dropdown from 'react-bootstrap/Dropdown';
+import { UserOutlined, LogoutOutlined } from '@ant-design/icons';
 
 const StaffHeader: React.FC = () => {
     const [isHovered, setIsHovered] = useState(false);
@@ -17,46 +17,18 @@ const StaffHeader: React.FC = () => {
         navigate('/');
         window.location.reload();
     };
+
     const hometag = () => {
         navigate('/');
-    }
+    };
+
     return (
         <div className="member-header-home">
             <nav>
+                <div>
+                    <img src={logo} alt="logo" className='logo' onClick={hometag} />
+                </div>
                 <ul>
-                    <div className="member-header-items">
-                        <li className="inline-block">
-                            <img src={logo} alt="logo" className='logo' onClick={() => hometag()} />
-                        </li>
-                    </div>
-                    <div className="member-header-items">
-                        <li className="inline-block">
-                            <Link to='/'>
-                                HOME
-                            </Link>
-                        </li>
-                    </div>
-                    <div className="member-header-items">
-                        <li className="inline-block">
-                            <ScrollLink to='member-auctions-content' spy={true} offset={-100} duration={500}>
-                                AUCTION
-                            </ScrollLink>
-                        </li>
-                    </div>
-                    <div className="member-header-items">
-                        <li className="inline-block">
-                            <ScrollLink to='member-jewel-content' spy={true} offset={-100} duration={500}>
-                                JEWELRY
-                            </ScrollLink>
-                        </li>
-                    </div>
-                    <div className="member-header-items">
-                        <li className="inline-block">
-                            <ScrollLink to='auctions-rule' spy={true} offset={-100} duration={500}>
-                                RULE
-                            </ScrollLink>
-                        </li>
-                    </div>
                     {user && (
                         <div className="member-header-items">
                             <li
@@ -71,9 +43,14 @@ const StaffHeader: React.FC = () => {
                                         <h3 className="user-name" style={{ margin: '0 0 0 5px' }}>{user.name}</h3>
                                     </Dropdown.Toggle>
                                     <Dropdown.Menu>
-
-                                        <Dropdown.Item href="/staffprofile">My Profile</Dropdown.Item>
-                                        <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
+                                        <Dropdown.Item href="/staffprofile">
+                                            <UserOutlined style={{ marginRight: '8px' }} />
+                                            My Profile
+                                        </Dropdown.Item>
+                                        <Dropdown.Item onClick={handleLogout}>
+                                            <LogoutOutlined style={{ marginRight: '8px' }} />
+                                            Logout
+                                        </Dropdown.Item>
                                     </Dropdown.Menu>
                                 </Dropdown>
                             </li>
