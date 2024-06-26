96d6688febabd080a96c8674f611aba9bc83cd24
hiepnt171646 <97937052+hiepnt171646@users.noreply.github.com>
2024-06-27 00:35:03 +0700

    staff commit

--- Frontend/Fortune/src/WebApplication/StaffPage/components/template/staff-jewelry/staff-jewelry.tsx
+++ Frontend/Fortune/src/WebApplication/StaffPage/components/template/staff-jewelry/staff-jewelry.tsx
@@ -2,7 +2,7 @@ import StaffViewJewelry from "../../molucules/jewelry-details/staffViewJewelry"
 function MemberJewelryBody() {
   return (
     <div> 
-    <StaffViewJewelry/>
+    <StaffViewJewelry/> 
     </div>
   )
 }
