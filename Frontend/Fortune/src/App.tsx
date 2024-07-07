c3b6efe22781dc91332f14a4d14535ebb9e53c6f
TangDuYen <YenTDSE171310@fpt.edu.vn>
2024-07-08 00:55:17 +0700

    add routes

--- Frontend/Fortune/src/App.tsx
+++ Frontend/Fortune/src/App.tsx
@@ -1,16 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
-/*-------------------------------AUTHENTICAL----------------------------------------------*/
+import { Route, Routes } from 'react-router-dom';
+import { Bounce, ToastContainer } from 'react-toastify';
 import GuestHomePg from './WebApplication/GuestPage/guest-home-page/guest-home-pg';
 import RegisterPg from './WebApplication/AccountRegister/components/pages/Register-pg';
-import { Route, Routes } from 'react-router-dom';
 import Loginpg from './WebApplication/AccountRegister/components/pages/Loginpg';
 import ForgotPass from './WebApplication/AccountRegister/components/pages/forgotPage';
-
-/*--------------------------MEMBER----------------------------------------------*/
 import MemeberDashBoardPG from './WebApplication/MemberPage/components/pages/member-dashboard-pg/memberdashboard';
 import MemberHomePg from './WebApplication/MemberPage/components/pages/member-home-pg/member-home-pg';
-import { UserProvider, useUser } from './WebApplication/Data/UserContext';
 import MemberAuctionPg from './WebApplication/MemberPage/components/pages/member-auction-pg/memberauctionPg';
 import JewelryUploadPg from './WebApplication/MemberPage/components/pages/member-jewelry-upload-pg/jewelry-upload-pg';
 import MemberJewelryPg from './WebApplication/MemberPage/components/pages/member-Jewelry-pg/member-jewelry-pg';
@@ -20,35 +17,32 @@ import MemberAucDetailsPg from './WebApplication/MemberPage/components/pages/mem
 import MemberRegisterJewelryAuctionPg from './WebApplication/MemberPage/components/pages/member-register-jewelry-auction/member-register-jewelry-auction-pg';
 import MemberJewDetailsPg from './WebApplication/MemberPage/components/pages/member-jewelry-details-pg/member-jewelry-details-pg';
 import StaffJewelryPg from './WebApplication/StaffPage/components/pages/staff-view-jewelry/staff-view-jewelry';
-import StaffViewJewelry from './WebApplication/StaffPage/components/molucules/jewelry-details/staffViewJewelry';
 import StaffViewJewelryPg from './WebApplication/StaffPage/components/pages/stafff-Jewelry-pg/staff-View-Jewelry-Details-Pg';
 import GuestAuctionDetailsPg from './WebApplication/GuestPage/components/guest-auction-details-page/guest-auction-details-pg';
 import ProfileStaff from './WebApplication/StaffPage/components/molucules/StaffProfile/ProfileStaff';
 import BiddingPG from './WebApplication/MemberPage/components/pages/member-bidding-pg/BiddingPG';
 import MemberMyBidsPg from './WebApplication/MemberPage/components/pages/member-my-bids-pg/member-my-bids-pg';
-import AdminPg from './WebApplication/AdminPage/page/AdminPg';
-import { Bounce, ToastContainer } from 'react-toastify';
+import { UserProvider, useUser } from './WebApplication/Data/UserContext'; // Ensure correct import
+import MemberAccountWalletPg from './WebApplication/MemberPage/components/pages/member-account-wallet-pg/member-account-wallet-pg';
+import MyWalletPg from './WebApplication/MemberPage/components/pages/member-my-wallet-pg/my-wallet-pg';
+import UpdateWalletPg from './WebApplication/MemberPage/components/pages/member-update-wallet-pg/update-wallet-pg';
 
-import WebSocketClient from './config/websocketClient'; // Import WebSocketClient
-
-import TableUser from './WebApplication/AdminPage/molecules/Users/TableUser';
 const App: React.FC = () => {
   const { user, setUser } = useUser();
-  const [wsClient, setWsClient] = useState<WebSocketClient | null>(null);
+  
 
   useEffect(() => {
-    const storedUser = sessionStorage.getItem("loginedUser");
+    const storedUser = sessionStorage.getItem('loginedUser');
     if (storedUser) {
       setUser(JSON.parse(storedUser));
     }
-    console.log(storedUser);
-    // const client = new WebSocketClient('wss://localhost:44361/ws'); // Initialize WebSocket
-    // setWsClient(client);
-
-    // return () => {
-    //   client.sendMessage('Client disconnected.');
-    // };
   }, [setUser]);
+
+  useEffect(() => {
+    if (user) {
+      console.log(user.role);
+    }
+  }, [user]);
 
   return (
     <>
@@ -64,7 +58,8 @@ const App: React.FC = () => {
         pauseOnHover={false}
         theme="light"
         className='toast-content'
-        transition={Bounce} />
+        transition={Bounce}
+      />
       <Routes>
         <Route path='/'>
           {user === null ? (
@@ -80,10 +75,7 @@ const App: React.FC = () => {
               <Route path='/login' element={<Loginpg />} />
               {user.role === 1 && (
                 <>
-                  <Route index element={<AdminPg />} />
-                 <Route index element={<AdminPg/>} />
-                 <Route path="/adminUser" element ={<TableUser/>} />
-
+                  
                 </>
               )}
               {user.role === 2 && (
@@ -98,9 +90,11 @@ const App: React.FC = () => {
                   <Route path='/userJewel/upload' element={<JewelryUploadPg />} />
                   <Route path='/userJewel' element={<MemberJewelryPg />} />
                   <Route path='/ViewJewInfo/:id' element={<MemberJewelryPg />} />
-                  {/* <Route path='/userJewel/:id' element={<MemberJewelryPg />} /> */}
                   <Route path="/update-jewelry/:id/:material" element={<MemberViewJewelryPg />} />
                   <Route path="/register-jewelry-auction/:id/:material" element={<MemberRegisterJewelryAuctionPg />} />
+                  <Route path="/register-wallet" element={<MemberAccountWalletPg />} />
+                  <Route path="/userWallet" element={<MyWalletPg />} />
+                  <Route path="/update-wallet" element={<UpdateWalletPg />} />
                 </>
               )}
               {user.role === 3 && (
@@ -108,12 +102,10 @@ const App: React.FC = () => {
                   <Route path='/' element={<StaffJewelryPg />} />
                   <Route path="/staff/update-jewelry/:id/:material" element={<StaffViewJewelryPg />} />
                   <Route path='/staffprofile' element={<ProfileStaff />} />
-                  {/* Add your staff routes here */}
                 </>
               )}
               {user.role === 5 && (
                 <>
-                  {/* Add your manager routes here */}
                   <Route index element={<ManagerHomePg />} />
                   <Route path='/managerA&DJew' element={<ProfileStaff />} />
                 </>
