1d008c074754dd3a40c30002c61d754a3e301c80
TangDuYen <YenTDSE171310@fpt.edu.vn>
2024-07-24 01:44:20 +0700

    resolve conffilct

--- Frontend/Fortune/src/WebApplication/MemberPage/components/template/member-bidding/biddingtmp.tsx
+++ Frontend/Fortune/src/WebApplication/MemberPage/components/template/member-bidding/biddingtmp.tsx
@@ -1,13 +1,8 @@
-import React from "react";
-import { Helmet } from "react-helmet"; // Ensure this import matches your Helmet package
 import MyBidding from "../../molecules/biddings/my-bids";
 
 function BiddingTmp() {
     return (
         <>
-            <Helmet>
-                <meta httpEquiv="refresh" content="5" />
-            </Helmet>
             <div>
                 <MyBidding />
             </div>
