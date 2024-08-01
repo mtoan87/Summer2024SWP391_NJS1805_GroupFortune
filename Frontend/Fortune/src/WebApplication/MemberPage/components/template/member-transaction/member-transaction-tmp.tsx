789a32f758207f1a49e8165bab2dede6da604f51
TangDuYen <YenTDSE171310@fpt.edu.vn>
2024-08-01 22:31:31 +0700

    update transaction

---
+++ Frontend/Fortune/src/WebApplication/MemberPage/components/template/member-transaction/member-transaction-tmp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyTransaction from '../../molecules/transaction/my-transaction'
+
+function MemberTransactionTmp() {
+  return (
+    <>
+    <MyTransaction />
+    </>
+  )
+}
+
+export default MemberTransactionTmp
